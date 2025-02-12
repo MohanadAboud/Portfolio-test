ece82764f14a59d7c2eff89428fc0ba340e22768
MohanadAboud <158301720+MohanadAboud@users.noreply.github.com>
2025-02-12 17:54:50 +0100

    Add files via upload

--- Portfolio/js/bubble.js
+++ Portfolio/js/bubble.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const customCursor = document.getElementById('customCursor')
 const interactiveElements = document.querySelectorAll('.card, .grid-item-four')
 
@@ -14,6 +16,28 @@ interactiveElements.forEach((element) => {
 
   element.addEventListener('mouseleave', () => {
     customCursor.style.opacity = '0'
+    element.style.cursor = 'auto'
+  })
+})
+
+// bubble til projekt delen
+
+const slideCursor = document.getElementById('slideCursor')
+const imgElements = document.querySelectorAll('.img-bubble')
+
+document.addEventListener('mousemove', (e) => {
+  slideCursor.style.top = `${e.clientY}px`
+  slideCursor.style.left = `${e.clientX}px`
+})
+
+imgElements.forEach((element) => {
+  element.addEventListener('mouseenter', () => {
+    slideCursor.style.opacity = '1'
+    element.style.cursor = 'none'
+  })
+
+  element.addEventListener('mouseleave', () => {
+    slideCursor.style.opacity = '0'
     element.style.cursor = 'auto'
   })
 })
